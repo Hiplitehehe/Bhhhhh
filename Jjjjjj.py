a443f91902df62bad1b317813c872fac4d5a59f2
Hiplitehehe <hiplitehehe@gmail.com>
2024-11-07 16:22:28 +0800

    Update and rename generated_key_887338032703868969_1730364107.txt to Jjjjjj.py

---
+++ Jjjjjj.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+import base64
+
+app = Flask(__name__)
+
+@app.route('/encode_base64', methods=['POST'])
+def encode_base64():
+    data = request.get_json()  # Get JSON data from the request
+    if 'text' in data:
+        text = data['text']
+        encoded_text = base64.b64encode(text.encode('utf-8')).decode('utf-8')
+        return jsonify({'encoded': encoded_text})
+    else:
+        return jsonify({'error': 'No text provided'}), 400
+
+if __name__ == '__main__':
+    app.run(debug=True)
