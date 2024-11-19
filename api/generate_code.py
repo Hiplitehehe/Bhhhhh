2979d7d3940fd305e509acb0f415bad0b51ca845
Hiplitehehe <hiplitehehe@gmail.com>
2024-11-19 10:13:50 +0800

    Update generate_code.py

--- api/generate_code.py
+++ api/generate_code.py
@@ -8,7 +8,7 @@ def generate_code():
     # Generates a random code in the format XXXX-XXXX-XXXX
     return '-'.join([''.join(random.choices(string.ascii_uppercase + string.digits, k=4)) for _ in range(4)])
 
-@app.route('/generate_code', methods=['GET'])
+@app.route('/', methods=['GET'])
 def get_code():
     # Returns the generated random code
     return jsonify({"code": generate_code()})
